cfc3848ac8915c18822f3618dd81de60edb30b5a
Hyunjin CHA <gump.cha@gmail.com>
2021-01-28 13:01:31 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Tesla from './Tesla';
+
 function App() {
   return (
     <div>
       <h1>Hello ReactJS</h1>
+      <Tesla model="Model X"/>
     </div>
   );
 }
---
+++ src/Tesla.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tesla(props) {
+  return (
+    <div>
+      <h3>I love Tesla!!!</h3>
+      <h3>My model is {props.model}</h3>
+    </div>
+  )
+}
+
+export default Tesla;
+
